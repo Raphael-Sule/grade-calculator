e55fc18b26a783f3eb8182d1dcb095c6dd531e3b
Raphael-Sule <157833794+Raphael-Sule@users.noreply.github.com>
2025-05-02 14:35:15 -0400

    final touches

--- src/test/java/com/example/gradecalculator/mutationTests.java
+++ src/test/java/com/example/gradecalculator/mutationTests.java
@@ -2,10 +2,12 @@
 package com.example.gradecalculator;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+
 import java.util.stream.Stream;
 import org.junit.jupiter.params.provider.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+
 
 public class mutationTests {
 
@@ -15,12 +17,19 @@ public class mutationTests {
         assertEquals(expectedGrade, GradeCalculator.calculateLetterGrade(score), description);
     }
 
-    private static Stream<Arguments> calculateLetterGradeMutationTestGenerator() {
+    private static Stream<Arguments> calculateLetterGradeMutationTestGenerator() 
+    {
         return Stream.of(
             Arguments.of("t1_line 31", 90.0, 'A'),
             Arguments.of("t1_line_34", 80.0, "B"),
             Arguments.of("t1_line_39", 70.0, "C"),
-            Arguments.of("t1_line_43", 60.0, "D")
+            Arguments.of("t1_line_43", 60.0, "D"),
+            
+            Arguments.of("t1_line 27", 100.0, 'A'),
+            Arguments.of("t1_line 27", 0.0, 'F')
         );
     }
+    
+
+
 }
